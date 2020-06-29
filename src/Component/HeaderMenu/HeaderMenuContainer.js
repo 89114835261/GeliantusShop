fe3bb1a130149bb07d1f888d5b7e3cc3b57ed65c
Denis_Bespalov <googlonator@gmail.com>
2020-06-29 05:42:19 +0300

    Зарефакторили большую часть стилей с помощью sass препроцессора

--- src/Component/HeaderMenu/HeaderMenuContainer.js
+++ src/Component/HeaderMenu/HeaderMenuContainer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import {SetMenuActionCreator, changeIsOpenMenuAC, SetMainMenuActionCreator} from '../redux/HeaderMenuReducer';
 import {wayLinkActionCreator, mutateStateActionCreator} from './../redux/Project-reducer';
-import H from './HeaderMenu.module.css';
+import H from './HeaderMenu.module.scss';
 import Button from './button/Button';
 import MainButton from './MainButton/MainButton';
 import { NavLink } from 'react-router-dom';
