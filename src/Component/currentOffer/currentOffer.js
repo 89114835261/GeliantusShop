68a96881a83b49cf283005e001cfc49fe8edc9bb
Denis_Bespalov <googlonator@gmail.com>
2020-07-22 18:46:20 +0300

    Перед установкой библиотке scrollTo

--- src/Component/currentOffer/currentOffer.js
+++ src/Component/currentOffer/currentOffer.js
@@ -4,12 +4,14 @@ import style from './currentOffer.module.scss';
 let CurrentOffer = (props) => {
     
     return(
-        <div className={style.currentOffer}>
-            <button onClick={() => props.scrollOfferBox('back')}>Назад</button>
-            <button onClick={() => props.scrollOfferBox('next')}>Вперед</button>
-            <div className={style.names}>{props.nameMap}</div>
-            <div ref={props.catWrapper} className={style.catWrapper}>{props.catMap}</div>
-            
+        <div className={style.wrapper}>
+            <h2>Актуальные предложения</h2>
+            <div className={style.currentOffer}>
+                <button onClick={() => props.scrollOfferBox('back')}>Назад</button>
+                <button onClick={() => props.scrollOfferBox('next')}>Вперед</button>
+                <div className={style.names}>{props.nameMap}</div>
+                <div ref={props.catWrapper} className={style.catWrapper}>{props.catMap}</div>
+            </div>
         </div>
     )
 }
